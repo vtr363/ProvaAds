a24a5fff9b67a5af9b146708cfcc139c7f4da499
vtr363 <victor.m.rocha.vr@gmail.com>
2023-10-31 17:53:02 -0300

    picker adicionado

--- app/p1/index.js
+++ app/p1/index.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { FlatList, StyleSheet, Text, View, Image} from "react-native";
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
 import axios from "axios";
+import { Picker } from '@react-native-picker/picker';
 
 const App = () => {
+  const [refrigerantes, setRefrigerantes] = useState([]);
   const [hamburgueres, setHamburgueres] = useState([]);
+  const [selectedRefrigerante, setSelectedRefrigerante] = useState("");
 
   useEffect(() => {
+    axios
+      .get("http://192.168.1.11:3000/produtos?categoria_id=2")
+      .then((response) => {
+        setRefrigerantes(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
     axios
       .get("http://192.168.1.11:3000/produtos?categoria_id=1")
       .then((response) => {
@@ -16,21 +28,38 @@ const App = () => {
       });
   }, []);
 
+  const handleRefrigeranteChange = (event) => {
+    setSelectedRefrigerante(event.target.value);
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.nome}>{item.nome}</Text>
+        <Text style={styles.descricao}>{item.descricao}</Text>
+        <Image
+          source={{ uri: item.imagemUrl }}
+          style={styles.imagem}
+        />
+        <Text style={styles.preco}>R$ {item.preco}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
+      <Picker
+        selectedValue={selectedRefrigerante}
+        onValueChange={(value) => setSelectedRefrigerante(value)}
+      >
+        <Picker.Item label={"Escolha um refrigerante"} value={""} />
+        {refrigerantes.map((refrigerante) => (
+          <Picker.Item key={refrigerante.id} label={refrigerante.nome} value={refrigerante.id} />
+        ))}
+      </Picker>
       <FlatList
         data={hamburgueres}
-        renderItem={({ item }) => (
-          <View style={styles.item}>
-            <Text style={styles.nome}>{item.nome}</Text>
-            <Text style={styles.descricao}>{item.descricao}</Text>
-            <Image
-              source={{ uri: item.imagemUrl }}
-              style={styles.imagem}
-            />
-            <Text style={styles.preco}>R$ {item.preco}</Text>
-          </View>
-        )}
+        renderItem={renderItem}
       />
     </View>
   );
